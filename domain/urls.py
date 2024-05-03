4ee24c0750946fbeff760bc65400411b03a37405
Fran <franrogel2003@gmail.com>
2024-05-03 19:42:54 -0300

    Urls game info update
    Se mejoro la url de gameInfo y se cambio la forma de acceder al template gameInfo desde games y listContent

--- domain/urls.py
+++ domain/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('register/', views.registerPage, name='register'),
     path('games/', views.games, name='games'),
-    path('games/gameInfo/<int:id>', views.gameInfo, name='game'),
+    path('gameInfo/<int:id>/', views.gameInfo, name='game'),
     path('users/', views.users, name='users'),
     path('users/profile/<int:id>', views.profile, name='profile'),
     path('users/profile/listContent/<int:id>', views.listContent, name='listContent'), #Preguntar como hago para evitar que se alargue la url
-    path('users/profile/listContent/game/<int:id>', views.gameInfo, name='game')
 ]
