d110598a11ac2713a194a669694f9a8c89d87449
Fran <franrogel2003@gmail.com>
2024-11-05 13:14:30 -0300

    Revert "mods wsgi.py"
    This reverts commit e92c2e1609fa148bc2efa8c3f95d49c3472c3b77.

--- integradorPrueba/wsgi.py
+++ integradorPrueba/wsgi.py
@@ -6,16 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
+
 import os
-import sys
-
-# Añade el directorio de tu proyecto al path
-path = '/home/frandev/LaboratorioApp'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Establece la variable de entorno para Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'integradorPrueba.settings'  # Asegúrate de que esto sea correcto
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'integradorPrueba.settings')
+
 application = get_wsgi_application()
